99b82f72c1d8d6c2fd74fe5f43cf6023daedbc47
Insindur <71377430+Insindur@users.noreply.github.com>
2020-10-09 09:32:01 +0200

    fix: fixed bad code in Main

--- src/no/noroff/sean/Main.java
+++ src/no/noroff/sean/Main.java
@@ -3,7 +3,7 @@ package no.noroff.sean;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Carole Baskin is an animal rights activist. ");
+        System.out.println("Carole Baskin might have fed her husband to the tigers ;)))) ");
         System.out.println("Change incoming");
     }
 }
