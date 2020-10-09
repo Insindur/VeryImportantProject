649225f3a3aeb9bdce697f7670e2dd67c7f6809a
Sean Skinner <Sean.Skinner@noroff.no>
2020-10-09 09:29:01 +0200

    feat: Adds extra functionality to Main

--- src/no/noroff/sean/Main.java
+++ src/no/noroff/sean/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Carole Baskin is an animal rights activist. ");
+        System.out.println("Change incoming");
     }
 }
